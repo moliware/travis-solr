d9ee77c60e795f5bc12d31cb50d91603906aa7ff
Colin Jones <trptcolin@gmail.com>
2013-01-23 22:31:53 -0600

    Fix folder name

--- travis-solr.sh
+++ travis-solr.sh
@@ -51,7 +51,7 @@ download_and_run() {
             dir_name="apache-solr-3.6.1"
             ;;
         3.6.2)
-            url="http://apache.rediris.es/lucene/solr/3.6.1/apache-solr-3.6.2.tgz"
+            url="http://apache.rediris.es/lucene/solr/3.6.2/apache-solr-3.6.2.tgz"
             dir_name="apache-solr-3.6.2"
             ;;
         4.0.0)
