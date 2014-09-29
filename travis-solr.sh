2c4b87a14ea7fc3410873dba844b63d692e20841
nickveenhof <nick.veenhof@gmail.com>
2014-09-29 11:37:17 +0200

    Adding multicore possibilities to add custom configs and cores

--- travis-solr.sh
+++ travis-solr.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
 SOLR_PORT=${SOLR_PORT:-8983}
+SOLR_VERSION=${SOLR_VERSION:-4.9.1}
+DEBUG=${DEBUG:-0}
+SOLR_CORE=${SOLR_CORE:-core0}
 
 download() {
-    echo "Downloading solr from $1..."
-    curl -s $1 | tar xz
-    echo "Downloaded"
+    FILE="$2.tgz"
+    if [ -f $FILE ];
+    then
+       echo "File $FILE exists."
+    else
+       echo "File $FILE does not exist. Downloading solr from $1..."
+       curl -O $1
+       tar -zxvf $FILE
+    fi
+    echo "Downloaded!"
 }
 
 is_solr_up(){
-    http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$SOLR_PORT/solr/admin/ping")`
+    echo "Checking if solr is up on http://localhost:$SOLR_PORT/solr/core0/admin/ping"
+    http_code=`echo $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$SOLR_PORT/solr/core0/admin/ping")`
     return `test $http_code = "200"`
 }
 
@@ -20,22 +31,26 @@ wait_for_solr(){
 }
 
 run() {
-    echo "Starting solr on port ${SOLR_PORT}..."
+    dir_name=$1
+    solr_port=$2
+    solr_core=$3
+    # Run solr
+    echo "Running with folder $dir_name"
+    echo "Starting solr on port ${solr_port}..."
 
+    # go to the solr folder
     cd $1/example
+
     if [ $DEBUG ]
     then
-        java -Djetty.port=$SOLR_PORT -jar start.jar &
+        java -Djetty.port=$solr_port -Dsolr.solr.home=multicore -jar start.jar &
     else
-        java -Djetty.port=$SOLR_PORT -jar start.jar > /dev/null 2>&1 &
+        java -Djetty.port=$solr_port -Dsolr.solr.home=multicore -jar start.jar > /dev/null 2>&1 &
     fi
     wait_for_solr
     cd ../../
+    curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=${solr_core}&instanceDir=${solr_core}&config=solrconfig.xml&schema=schema.xml&dataDir=data"
     echo "Started"
-}
-
-post_some_documents() {
-    java -Dtype=application/json -Durl=http://localhost:$SOLR_PORT/solr/update/json -jar $1/example/exampledocs/post.jar $2
 }
 
 
@@ -142,34 +157,62 @@ download_and_run() {
             dir_conf="collection1/conf/"
             ;;
         4.9.1)
-            url="http://archive.apache.org/dist/lucene/solr/4.9.1/solr-4.9.1.tgz"
+            url="http://mirror.nl.webzilla.com/apache/lucene/solr/4.9.1/solr-4.9.1.tgz"
             dir_name="solr-4.9.1"
             dir_conf="collection1/conf/"
             ;;
     esac
 
-    download $url
+    download $url $dir_name
+    add_core $dir_name $dir_conf $SOLR_CORE $SOLR_CONFS
+    run $dir_name $SOLR_PORT $SOLR_CORE
+
+    if [ -z "${SOLR_DOCS}" ]
+    then
+        echo "$solr_docs not defined, skipping initial indexing"
+    else
+        post_documents $dir_name $SOLR_DOCS $SOLR_CORE $SOLR_PORT
+    fi
+}
+
+add_core() {
+    dir_name=$1
+    dir_conf=$2
+    solr_core=$3
+    solr_confs=$4
+    # prepare our folders
+    [[ -d "${dir_name}/example/multicore/${solr_core}" ]] || mkdir $dir_name/example/multicore/$solr_core
+    [[ -d "${dir_name}/example/multicore/${solr_core}/conf" ]] || mkdir $dir_name/example/multicore/$solr_core/conf
 
     # copies custom configurations
-    for file in $SOLR_CONFS
-    do
-        if [ -f $file ]
-        then
-            cp $file $dir_name/example/solr/$dir_conf
-            echo "Copied $file into solr conf directory."
-        fi
-    done
-
-    # Run solr
-    run $dir_name $SOLR_PORT
-
-    # Post documents
-    if [ -z "$SOLR_DOCS" ]
-    then
-        echo "$SOLR_DOCS not defined, skipping initial indexing"
+    if [ -d "${solr_confs}" ] ; then
+      cp -R $solr_confs/* $dir_name/example/multicore/$solr_core/conf/
     else
-        echo "Indexing $SOLR_DOCS"
-        post_some_documents $dir_name $SOLR_DOCS
+      for file in $solr_confs
+      do
+        if [ -f "${file}" ]; then
+            cp $file $dir_name/example/multicore/$solr_core/conf
+            echo "Copied $file into solr conf directory."
+        else
+            echo "${file} is not valid";
+            exit 1
+        fi
+      done
+    fi
+}
+
+post_documents() {
+    dir_name=$1
+    solr_docs=$2
+    solr_core=$3
+    solr_port=$4
+      # Post documents
+    if [ -z "${solr_docs}" ]
+    then
+        echo "$solr_docs not defined, skipping initial indexing"
+    else
+        echo "Indexing $solr_docs"
+        java -Dtype=application/json -Durl=http://localhost:$solr_port/solr/$solr_core/update/json -jar $dir_name/example/exampledocs/post.jar $solr_docs
     fi
 }
 
@@ -182,7 +225,6 @@ check_version() {
             ;;
     esac
 }
-
 
 check_version $SOLR_VERSION
 download_and_run $SOLR_VERSION
